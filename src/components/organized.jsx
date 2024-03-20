1c5cad64ebbb4555e2d3b7931a1c487a437dfd59
unknown <youngscientist.iysa@gmail.com>
2024-03-20 15:55:46 +0700

    fourt  commit

--- src/components/organized.jsx
+++ src/components/organized.jsx
@@ -3,7 +3,7 @@ function Organized() {
         <>
         <section className="organized-section">
             <div class="container">
-                <h1 class="mb-3">ORGANIZED BY :</h1>
+                <h1 class="mb-3">Diselenggarakan oleh :</h1>
                 <div class="row logos">
                     <div class="wow fadeInUp mt-4">
                         <a href="#"><img src="assets/logo/Iysa.png" alt="" class=" img-fluid" /></a>
@@ -13,7 +13,7 @@ function Organized() {
                         <a href="#"><img src="assets/logo/mahsa.png" alt="" class=" img-fluid" /></a>
                     </div> */}
                 </div>
-                {/* <h1 class="mt-5 mb-2">SUPPORTED BY :</h1>
+                {/* <h1 class="mt-5 mb-2">Didukung oleh :</h1>
                 <div class="row logos2">
                     <div class="wow fadeInUp">
                         <a href="#"><img src="assets/logo/miica.png" alt="" class=" img-fluid" /></a>
